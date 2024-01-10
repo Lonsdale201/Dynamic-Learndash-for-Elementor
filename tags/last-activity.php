d0957491e0edb75e2f4a85ea2eb15c984d1bff11
Soczó <lonsdale201@hotmail.com>
2024-01-10 23:07:40 +0100

    Update last-activity.php

--- tags/last-activity.php
+++ tags/last-activity.php
@@ -26,10 +26,36 @@ class Last_Activity extends \Elementor\Core\DynamicTags\Tag {
         return [ \Elementor\Modules\DynamicTags\Module::TEXT_CATEGORY ];
     }
 
+    protected function _register_controls() {
+        $this->add_control(
+            'date_format',
+            [
+                'label' => __( 'Format', 'elementor-pro' ),
+                'type' => \Elementor\Controls_Manager::SELECT,
+                'options' => [
+                    'default' => __( 'Default', 'elementor-pro' ),
+                    'custom' => __( 'Custom', 'elementor-pro' ),
+                ],
+                'default' => 'default',
+            ]
+        );
+
+        $this->add_control(
+            'custom_date_format',
+            [
+                'label' => __( 'Custom Date Format', 'elementor-pro' ),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'condition' => [
+                    'date_format' => 'custom',
+                ],
+                'placeholder' => 'Y-m-d H:i:s',
+            ]
+        );
+    }
+
     public function render() {
         global $post;
     
-        // check course cpt
         if ( 'sfwd-courses' !== get_post_type( $post ) ) {
             echo '';
             return;
@@ -37,6 +63,10 @@ class Last_Activity extends \Elementor\Core\DynamicTags\Tag {
     
         $course_id = $post->ID;
         $user_id = get_current_user_id();
+    
+        $settings = $this->get_settings();
+        $date_format_option = $settings['date_format'];
+        $custom_date_format = $settings['custom_date_format'];
     
         if ( empty( $user_id ) ) {
             echo '';
@@ -47,7 +77,7 @@ class Last_Activity extends \Elementor\Core\DynamicTags\Tag {
             'course_id' => $course_id,
             'post_id' => $course_id,
             'user_id'   => $user_id,
-            'activity_type' => 'course', // activity type: course
+            'activity_type' => 'course', 
             'order'     => 'DESC',
             'per_page'  => 1
         );
@@ -55,20 +85,22 @@ class Last_Activity extends \Elementor\Core\DynamicTags\Tag {
         $activity = learndash_get_user_activity( $args );
     
         if ( !empty( $activity ) ) {
-            // select latest activity
             $last_activity_date = $activity->activity_updated;
     
             if ( !empty( $last_activity_date ) ) {
-                // format to wp default settings
-                $date_format = apply_filters( 'learndash_date_time_formats', get_option( 'date_format' ) . ' ' . get_option( 'time_format' ) );
+                $date_format = 'custom' === $date_format_option && !empty($custom_date_format)
+                                ? $custom_date_format
+                                : get_option( 'date_format' ) . ' ' . get_option( 'time_format' );
+    
                 $formatted_date = date_i18n( $date_format, $last_activity_date );
                 echo esc_html( $formatted_date );
             } else {
-                echo ''; // no activity
+                echo ''; 
             }
         } else {
-            echo ''; //  no activity
+            echo ''; 
         }
     }
+    
     
 }
