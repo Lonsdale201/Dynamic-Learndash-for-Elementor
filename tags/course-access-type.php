c38436d14b000e904ec5f7d51791a2e40976d4cc
Soczó <lonsdale201@hotmail.com>
2024-01-10 23:07:25 +0100

    Update course-access-type.php

--- tags/course-access-type.php
+++ tags/course-access-type.php
@@ -5,10 +5,9 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-
 use Elementor\Core\DynamicTags\Tag;
 
-class Course_Access_Type extends \Elementor\Core\DynamicTags\Tag {
+class Course_Access_Type extends Tag {
 
     public function get_name() {
         return 'course-access-type';
@@ -28,15 +27,35 @@ class Course_Access_Type extends \Elementor\Core\DynamicTags\Tag {
 
     protected function _register_controls() {
         $this->add_control(
-            'visibility',
+            'format_type',
             [
-                'label' => __( 'Visibility', 'elementor-pro' ),
+                'label' => __( 'Format Type', 'elementor-pro' ),
                 'type' => \Elementor\Controls_Manager::SELECT,
                 'options' => [
-                    'everyone' => __( 'Everybody see', 'elementor-pro' ),
-                    'enrolled' => __( 'Only Enrolled see', 'elementor-pro' ),
+                    'default' => __( 'Learndash Default', 'elementor-pro' ),
+                    'custom' => __( 'Custom Format', 'elementor-pro' ),
                 ],
-                'default' => 'everyone',
+                'default' => 'default',
+            ]
+        );
+
+        $this->add_text_control_for_course_access_type('open', __( 'Open', 'elementor-pro' ));
+        $this->add_text_control_for_course_access_type('free', __( 'Free', 'elementor-pro' ));
+        $this->add_text_control_for_course_access_type('paynow', __( 'Buy Now', 'elementor-pro' ));
+        $this->add_text_control_for_course_access_type('subscribe', __( 'Subscription', 'elementor-pro' ));
+        $this->add_text_control_for_course_access_type('closed', __( 'Closed', 'elementor-pro' ));
+        $this->add_text_control_for_course_access_type('enrolled', __( 'Enrolled', 'elementor-pro' ));
+    }
+
+    private function add_text_control_for_course_access_type($type, $label) {
+        $this->add_control(
+            "custom_text_{$type}",
+            [
+                'label' => $label,
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'condition' => [
+                    'format_type' => 'custom',
+                ],
             ]
         );
     }
@@ -44,45 +63,47 @@ class Course_Access_Type extends \Elementor\Core\DynamicTags\Tag {
     public function render() {
         global $post;
 
-        
         if ( 'sfwd-courses' !== get_post_type( $post ) ) {
             echo '';
             return;
         }
 
         $settings = $this->get_settings();
-        $visibility = $settings['visibility'];
+        $format_type = $settings['format_type'];
         $course_id = $post->ID;
+        $user_id = get_current_user_id();
 
-        if ( 'enrolled' === $visibility && ! sfwd_lms_has_access( $course_id, get_current_user_id() ) ) {
-            echo '';
-            return;
+        $is_user_enrolled = sfwd_lms_has_access( $course_id, $user_id );
+        $access_type = learndash_get_course_meta_setting($course_id, 'course_price_type');
+
+        if ($is_user_enrolled && $format_type === 'custom' && !empty($settings['custom_text_enrolled'])) {
+            echo esc_html( $settings['custom_text_enrolled'] );
+        } else {
+            $access_type_text = $this->get_access_type_text($access_type, $settings, $format_type);
+            echo esc_html( $access_type_text );
+        }
+    }
+
+    private function get_access_type_text($access_type, $settings, $format_type) {
+        if ($format_type === 'custom' && isset($settings["custom_text_{$access_type}"])) {
+            return $settings["custom_text_{$access_type}"];
         }
 
-        $access_type = learndash_get_course_meta_setting($course_id, 'course_price_type');
-        $access_type_text = '';
-
-       
+        // Default texts
         switch ($access_type) {
             case 'open':
-                $access_type_text = __( 'Open', 'learndash' );
-                break;
+                return __( 'Open', 'learndash' );
             case 'free':
-                $access_type_text = __( 'Free', 'learndash' );
-                break;
+                return __( 'Free', 'learndash' );
             case 'paynow':
-                $access_type_text = __( 'Buy Now', 'learndash' );
-                break;
+                return __( 'Buy Now', 'learndash' );
             case 'subscribe':
-                $access_type_text = __( 'Subscription', 'learndash' );
-                break;
+                return __( 'Subscription', 'learndash' );
             case 'closed':
-                $access_type_text = __( 'Closed', 'learndash' );
-                break;
+                return __( 'Closed', 'learndash' );
             default:
-                $access_type_text = __( 'Unknown', 'learndash' );
+                return __( 'Unknown', 'learndash' );
         }
-
-        echo esc_html( $access_type_text );
     }
+
 }
